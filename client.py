c4eeba3f494f1c6d25915621085bc093bf5bc570
ShreyansK2000 <gshreyansk4@gmail.com>
2020-10-07 13:26:28 -0700

    sharing public key and session key working now

--- client.py
+++ client.py
@@ -1,16 +1,36 @@
 import socket
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import base64
 
 
-def do_client():
+def do_client():    
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
-    BUFFER_SIZE = 1024
-    MESSAGE = "Hello, World!"
+    BUFFER_SIZE = 4096
+    INIT_MESSAGE = "HELLO"
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(MESSAGE.encode('utf-8'))
+    s.send(INIT_MESSAGE.encode('utf-8'))
     data = s.recv(BUFFER_SIZE)
+
+    serverPublicKey = data
+    # print(serverPublicKey)
+
+    #generate session key
+    session_key = get_random_bytes(16)
+    cipher_rsa = PKCS1_OAEP.new(RSA.import_key(serverPublicKey))
+    enc_session_key = cipher_rsa.encrypt(session_key)
+
+    s.send(enc_session_key)
+
+    # print("session key: ", session_key)
+    # print("encrypted key: ", enc_session_key)
+
+
+
     s.close()
 
-    print("received data:", data.decode('utf-8'))
+    # print("received data:", data.decode('utf-8'))
--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
 import socket
-
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import base64
 
 def do_server():
+
+    # RSA keygen
+    key = RSA.generate(2048)
+
+    cipher_rsa = PKCS1_OAEP.new(key)
+    
+    public_key = key.publickey().export_key()
+
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
-    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+    BUFFER_SIZE = 4096  # Normally 1024, but we want fast response
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
@@ -16,7 +27,15 @@ def do_server():
     print('Connection address:', addr)
     while 1:
         data = conn.recv(BUFFER_SIZE)
-        if not data: break
-        print("received data:", data.decode('utf-8'))
-        conn.send(data)  # echo
+        if data.decode("utf-8") == "HELLO":
+            print("received connection request")
+            print("sending public RSA key")
+        
+            conn.send(public_key)  # echo
+            encrypted_session_key = conn.recv(BUFFER_SIZE)
+            session_key = cipher_rsa.decrypt(encrypted_session_key)
+            # print("received session key:", session_key)
+            # print("received encr session key:",  encrypted_session_key)
+
+        
     conn.close()
