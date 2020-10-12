caaeaa8a4150b2eb1040bb6a32994084dcb75e3a
ShreyansK2000 <gshreyansk4@gmail.com>
2020-10-11 17:47:13 -0700

    removed __pycache__ and added gui start up

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import base64
 
 
 def do_client():    
+    print("doing client")
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
     BUFFER_SIZE = 4096
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import server
 import client
 import sys
+from tkinter import *
 
+root = Tk()
+root.title("Team TBD's VPN")
+isServerApp = BooleanVar()
 
+def launch():
+    global isServerApp, root
+    root.destroy()
+    server.do_server() if isServerApp.get() else client.do_client()
 
 if __name__ == "__main__":
-    print(sys.argv[0])
-
-    numArgs = len(sys.argv)
-    if numArgs != 2:
-        print("incorrect number of arguments, please specify client or server with -S or -C flags")
-        exit()
-
-    if sys.argv[1] != "-S" and sys.argv[1] != "-C":
-        print("incorrect second arg, please specify client or server with -S or -C flags")
+      
+  
+    frame1 = Frame(root)
+    frame1.pack()
     
-    if sys.argv[1] == "-S":
-        server.do_server()
+    label1 = Label(frame1, text="Client or Server app?")
+    label1.pack()
 
-    if sys.argv[1] == "-C":
-        client.do_client()
+    radio1 = Radiobutton(frame1, text="Client", variable=isServerApp, value=False)
+    radio1.pack()
+
+    radio2 = Radiobutton(frame1, text="Server", variable=isServerApp, value=True)
+    radio2.pack()
+
+    button1 = Button(frame1, text="Select", command=launch)
+    button1.pack()
+
+    root.mainloop()
+
