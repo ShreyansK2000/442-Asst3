08be1bef1961b93e5b5e609e55562d721a1df779
Amy Liu <amyliu1.618@gmail.com>
2020-10-14 18:12:22 -0600

    completed initial authentication process

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 import base64
-import time
+from time import time
+import struct
 
 
 def do_client():    
@@ -11,31 +12,48 @@ def do_client():
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
     BUFFER_SIZE = 4096
-    INIT_MESSAGE = "I_CLIENT"
+    INIT_MESSAGE = "I_AM_CLIENT"
 
-    file_in = open("shared-secret.bin", "rb")
-    # ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
-    # timestamp = int(time.time())
-    # let's assume that the key is somehow available again
-    # cipher = AES.new(key, AES.MODE_EAX, nonce)
-    # data = cipher.decrypt_and_verify(ciphertext, tag)
+    # get shared secret
+    file_in = open("keys/shared-secret.bin", "rb")
+    secret_key = file_in.read(32)
+    iv = file_in.read(16)
+    iv2 = file_in.read(16)
+    file_in.close()
+    print(secret_key)
+    cipher = AES.new(secret_key, AES.MODE_OFB, iv)
 
-    clientKeys = RSA.import_key(open('./keys/client_keys/client-full-pair.pem'))
-    serverPublicKey = RSA.import_key(open('./keys/client_keys/client-server-public.pem'))
-   
+    # connect socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(INIT_MESSAGE.encode('utf-8'))
-    data = s.recv(BUFFER_SIZE)
 
-    # TODO data is something else for authentication
-    serverPublicKey = data
-
+    # create initial message
+    # nonce timestamp
+    timestamp = int(time())    
     #generate session key
-    sessionKey = get_random_bytes(32)
-    cipheRSA = PKCS1_OAEP.new(RSA.import_key(serverPublicKey))
-    encSessionKey = cipheRSA.encrypt(sessionKey)
+    session_key = get_random_bytes(32)
+    # encrypt bytestream
+    print("timestamp: " + str(timestamp))
+    auth_msg = struct.pack(">ix", timestamp) + session_key
+    print(session_key)
+    # print("num bytes: " + str(struct.pack(">i", timestamp).size))
+    ciphertext = cipher.encrypt(auth_msg)
+    
+    # send id message
+    s.send(INIT_MESSAGE.encode('utf-8'))
+    # send encrypted nonce and key
+    s.send(ciphertext)
 
-    s.send(encSessionKey)
+    # get nonce back
+    data = s.recv(BUFFER_SIZE)
+    cipher = AES.new(secret_key, AES.MODE_OFB, iv2)
+    plaintext = cipher.decrypt(data)
+    (ret_timestamp, ) = struct.unpack(">i", plaintext[0:4])
+    # time skew? offer 3 seconds???
+    if ret_timestamp < timestamp + 3:
+        print("authenticated server") 
+    # TODO: what happens if not authenticated?
+
+
 
     s.close()
--- server.py
+++ server.py
@@ -5,20 +5,25 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 import base64
+import time
+import struct
 
 def do_server():
-
-    # RSA keygen
-    key = RSA.generate(2048)
-
-    cipher_rsa = PKCS1_OAEP.new(key)
-    
-    public_key = key.publickey().export_key()
-
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
-    BUFFER_SIZE = 4096  # Normally 1024, but we want fast response
+    BUFFER_SIZE = 4096  # Normally 1024, but we want fast response    
+    INIT_MESSAGE = "I_AM_CLIENT"
 
+    # get shared secret
+    file_in = open("keys/shared-secret.bin", "rb")
+    secret_key = file_in.read(32)
+    iv = file_in.read(16)
+    iv2 = file_in.read(16)
+    file_in.close()
+    print(secret_key)
+    cipher = AES.new(secret_key, AES.MODE_OFB, iv)
+
+    # connect socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
@@ -27,15 +32,23 @@ def do_server():
     print('Connection address:', addr)
     while 1:
         data = conn.recv(BUFFER_SIZE)
-        if data.decode("utf-8") == "HELLO":
+        if data.decode("utf-8") == INIT_MESSAGE:
             print("received connection request")
-            print("sending public RSA key")
-        
-            conn.send(public_key)  # echo
-            encrypted_session_key = conn.recv(BUFFER_SIZE)
-            session_key = cipher_rsa.decrypt(encrypted_session_key)
-            # print("received session key:", session_key)
-            # print("received encr session key:",  encrypted_session_key)
+            data = conn.recv(BUFFER_SIZE)
+            plaintext = cipher.decrypt(data)
+            print("authenticated client") 
+            # get nonce value
+            (timestamp, ) = struct.unpack(">i", plaintext[0:4])
+
+            # get session key
+            session_key = plaintext[5:]
+
+            # return nonce + 1
+            cipher = AES.new(secret_key, AES.MODE_OFB, iv2)
+            auth_return = struct.pack(">ix", int(timestamp) + 1)
+            ciphertext = cipher.encrypt(auth_return)
+            conn.send(ciphertext)
+
 
         
     conn.close()
